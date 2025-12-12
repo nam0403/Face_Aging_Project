c238eb85a45c4f05bc96ce0f30f879b3cea20fef
Nam Đinh <nam.dinh.phuong@preciofishbone.se>
2025-12-12 19:14:07 +0700

    update run

--- face_alignment/align.py
+++ face_alignment/align.py
@@ -7,7 +7,8 @@ from PIL import Image
 from tqdm import tqdm
 import random
 from datetime import datetime
-mtcnn_model = mtcnn.MTCNN(device='cuda:0', crop_size=(112, 112))
+import torch
+mtcnn_model = mtcnn.MTCNN(device= 'cuda' if torch.cuda.is_available() else 'cpu' , crop_size=(112, 112))
 
 def add_padding(pil_img, top, right, bottom, left, color=(0,0,0)):
     width, height = pil_img.size
